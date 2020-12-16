86f81f9b4913294281a01e524531679b25ad7d15
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2020-12-16 10:08:31 +0200

    add commit

---
+++ build/precache-manifest.3d9febe31a36ef879ccdc46ac4e91163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3025bee71065277a22464761a5e045ea",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "30895b20ce6be648ff48",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf8d5fe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf8d5fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f666bb2317ef1f0f4d",
+    "url": "/goit-react-hw-02-feedback/static/js/main.aa8e2d7b.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
